240d103d8356105fce836ee3f4ce8de3319a911a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-07 20:38:29 +0400

    ok

---
+++ core/src/com/grush/larcas/BreakableBlock.java
@@ -0,0 +1,23 @@
+package com.grush.larcas;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class BreakableBlock extends Block {
+    private boolean isBroken = false;
+
+    public BreakableBlock(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public Texture getTexture() {
+        return isBroken ? TexMaster.INSTANCE.BrokenBlockTex : TexMaster.INSTANCE.BreakableBlockTex;
+    }
+
+    @Override
+    public void hit() {
+        if (!isBroken) {
+            isBroken = true;
+        }
+    }
+}
